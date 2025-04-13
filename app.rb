475d9d017b40bdfb3529059314df91bf6aaffeae
martaf793 <martaf@uchicago.edu>
2025-04-13 23:32:18 +0000

    dynamic fire

--- app.rb
+++ app.rb
@@ -4,33 +4,46 @@ require "sinatra/reloader"
 get("/") do
   erb(:home)
 end
-get("/rock") do
-  moves=["rock", "paper", "scissors"]
-  @computer_play=moves.sample
-  if @computer_play=="rock"
-    @outcome="tied"
-  elsif @computer_play=="paper"
-    @outcome="lost"
-  else 
-    @outcome="won"
-  end
-  erb(:rock)
-end
+# get("/rock") do
+#   moves=["rock", "paper", "scissors"]
+#   @computer_play=moves.sample
+#   if @computer_play=="rock"
+#     @outcome="tied"
+#   elsif @computer_play=="paper"
+#     @outcome="lost"
+#   else 
+#     @outcome="won"
+#   end
+#   erb(:rock)
+# end
 
-get("/paper") do
-  moves=["rock", "paper", "scissors"]
-  @computer_play=moves.sample
-  if @computer_play=="paper"
-    @outcome="tied"
-  elsif @computer_play=="scissors"
-    @outcome="lost"
-  else 
-    @outcome="won"
-  end
-  erb(:paper)
-end
+# get("/paper") do
+#   moves=["rock", "paper", "scissors"]
+#   @computer_play=moves.sample
+#   if @computer_play=="paper"
+#     @outcome="tied"
+#   elsif @computer_play=="scissors"
+#     @outcome="lost"
+#   else 
+#     @outcome="won"
+#   end
+#   erb(:paper)
+# end
 
-get("/scissors") do
+# get("/scissors") do
+#   moves=["rock", "paper", "scissors"]
+#   @computer_play=moves.sample
+#   if @computer_play=="scissors"
+#     @outcome="tied"
+#   elsif @computer_play=="paper"
+#     @outcome="won"
+#   else 
+#     @outcome="lost"
+#   end
+#   erb(:scissors)
+# end
+get("/:action") do
+  @our_play=params.fetch("action")
   moves=["rock", "paper", "scissors"]
   @computer_play=moves.sample
   if @computer_play=="scissors"
@@ -40,5 +53,5 @@ get("/scissors") do
   else 
     @outcome="lost"
   end
-  erb(:scissors)
+  erb(:flexible)
 end
