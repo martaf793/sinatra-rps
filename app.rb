9bb67fa57a58ed99abf0cc89b3f79e51621c4511
martaf793 <martaf@uchicago.edu>
2025-04-13 21:54:04 +0000

    views

--- app.rb
+++ app.rb
@@ -7,3 +7,41 @@ get("/") do
   <p>Define some routes in app.rb</p>
   "
 end
+get("/rock") do
+  moves=["rock", "paper", "scissors"]
+  @computer_play=moves.sample
+  if @computer_play=="rock"
+    @outcome="tied"
+  elsif @computer_play=="paper"
+    @outcome="lost"
+  else 
+    @outcome="won"
+  end
+  erb(:rock)
+end
+
+get("/paper") do
+  moves=["rock", "paper", "scissors"]
+  @computer_play=moves.sample
+  if @computer_play=="paper"
+    @outcome="tied"
+  elsif @computer_play=="scissors"
+    @outcome="lost"
+  else 
+    @outcome="won"
+  end
+  erb(:paper)
+end
+
+get("/scissors") do
+  moves=["rock", "paper", "scissors"]
+  @computer_play=moves.sample
+  if @computer_play=="scissors"
+    @outcome="tied"
+  elsif @computer_play=="paper"
+    @outcome="won"
+  else 
+    @outcome="lost"
+  end
+  erb(:scissors)
+end
