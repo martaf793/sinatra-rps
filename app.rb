3e34e50687f394798220d6206d0a9ccc89bc7137
martaf793 <martaf@uchicago.edu>
2025-04-14 00:50:46 +0000

    learning_unless

--- app.rb
+++ app.rb
@@ -46,12 +46,43 @@ get("/:action") do
   @our_play=params.fetch("action")
   moves=["rock", "paper", "scissors"]
   @computer_play=moves.sample
-  if @computer_play=="scissors"
+  if @computer_play== @our_play
     @outcome="tied"
-  elsif @computer_play=="paper"
+  elsif (@our_play == "scissors" and @computer_play=="paper") or (@our_play == "paper" and @computer_play=="rock") or (@our_play == "rock" and @computer_play=="scissors")
     @outcome="won"
+  elsif @our_play == "fire"
+    @outcome="won, fire beats everything"     
+  elsif (@our_play!= "rock") and (@our_play!= "paper")  and (@our_play!= "scissors") and (@our_play!= "fire") 
+    @outcome= "lost, invalid action"
   else 
     @outcome="lost"
   end
   erb(:flexible)
 end
+
+#ALSO WITH UNLESS - STUDY BUDDY
+# get("/:action") do
+#   @our_play = params.fetch("action")
+#   moves = ["rock", "paper", "scissors"]
+
+#   # Check for invalid action right away.
+#   unless moves.include?(@our_play) or @our_play == "fire"
+#     @outcome = "lost, invalid action"
+#   else
+#     @computer_play = moves.sample
+
+#     if @computer_play == @our_play
+#       @outcome = "tied"
+#     elsif @our_play == "fire"
+#       @outcome = "won, fire beats everything"
+#     elsif (@our_play == "scissors" and @computer_play == "paper") or
+#           (@our_play == "paper" and @computer_play == "rock") or
+#           (@our_play == "rock" and @computer_play == "scissors")
+#       @outcome = "won"
+#     else 
+#       @outcome = "lost"
+#     end
+#   end
+
+#   erb(:flexible)
+# end
